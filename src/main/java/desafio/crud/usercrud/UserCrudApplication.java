30b0b6b7af0ffbdf9ae2833c283f91d4e9309266
MarioLuiz <marioluiz.arruda@gmail.com>
2023-05-11 14:16:46 -0400

    feat: adicionando command line runner

--- src/main/java/desafio/crud/usercrud/UserCrudApplication.java
+++ src/main/java/desafio/crud/usercrud/UserCrudApplication.java
@@ -1,13 +1,18 @@
 package desafio.crud.usercrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserCrudApplication {
+public class UserCrudApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserCrudApplication.class, args);
 	}
+	
+	@Override
+    public void run(String... args) throws Exception {
+    }
 
 }
