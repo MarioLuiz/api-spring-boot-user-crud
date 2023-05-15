1fdf9012fb47f3090846ca7e1fc20737bdf9ffb7
MarioLuiz <marioluiz.arruda@gmail.com>
2023-05-15 11:51:57 -0400

    fix: colocando verbo correto

--- src/main/java/desafio/crud/usercrud/controller/UsuarioController.java
+++ src/main/java/desafio/crud/usercrud/controller/UsuarioController.java
@@ -4,30 +4,32 @@ import java.text.ParseException;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import desafio.crud.usercrud.domain.Usuario;
 import desafio.crud.usercrud.dto.UsuarioDTO;
 import desafio.crud.usercrud.services.impl.UsuarioServiceImpl;
 
-@RestController
+@Controller
 @RequestMapping(value="/usuarios")
 public class UsuarioController {
 	
 	private UsuarioServiceImpl service;
 	
+	@Autowired
 	public UsuarioController(UsuarioServiceImpl service) {
 		this.service = service;
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody UsuarioDTO objDto) throws ParseException {
 		Usuario userValidado = service.validaUsuario(objDto);
 		userValidado = service.inserir(userValidado);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.created(null).build();
     }
 }
